c3718b594c2d7e492b0ad979d8ab260570d981ec
ghashghaei <61688969+ghashghaei@users.noreply.github.com>
2021-07-11 16:42:18 +0200

    add view

---
+++ resources/views/users.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{$title}}</title>
+
+    <!-- Styles -->
+    <!-- CSS only -->
+    <link rel="stylesheet" href="{{asset('public/assets/css/bootstrap.css')}}" />
+    <link rel="stylesheet" href="{{asset('public/assets/css/style.css')}}" />
+
+
+
+
+</head>
+<body>
+
+<style>
+    table, th, td {
+        border: 1px solid black;
+    }
+    th {
+        cursor: pointer;
+    }
+</style>
+
+<div class="container row mcenter">
+
+    <table class="main_tbl col-12">
+        <tr class="title_t">
+            <th>id </th>
+            <th>first name <button class="sort_btn">sorting</button> </th>
+            <th>last name <button class="sort_btn">sorting</button> </th>
+            <th>email address <button class="sort_btn">sorting</button> </th>
+            <th>Registered Date <button class="sort_btn">sorting</button> </th>
+        </tr>
+
+
+        @foreach($users as $user)
+            <tr class="title_b">
+                <td>{{ $user->id  }}</td>
+                <td>{{ $user->first_name }}</td>
+                <td>{{ $user->last_name }}</td>
+                <td>{{ $user->email_address }}</td>
+                <td>{{ $user->registered_date }}</td>
+            </tr>
+        @endforeach
+
+    </table>
+
+
+</div>
+
+<script src="{{asset('public/assets/js/jquery-3.6.0.min.js')}}"></script>
+<script src="{{asset('public/assets/js/bootstrap.js')}}"></script>
+<script src="{{asset('public/assets/js/my_js.js')}}"></script>
+</body>
+
+</html>
